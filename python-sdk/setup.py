29f928891bad0a31641fbc572604b6b8c8bdaff6
Raymond Artin <Raymondartin@languaza.net>
2026-07-20 06:05:05 +0300

    chore: release v1.1.1

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aegis-proxy-sdk",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=[
         "psycopg2-binary",
