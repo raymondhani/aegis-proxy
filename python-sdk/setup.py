83eb74623ff8ed650426b84d5e0d37b6b56bd56a
Raymond Artin <Raymondartin@languaza.net>
2026-07-23 22:23:10 +0300

    chore(sdk): bump version to 1.2.0

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aegis-proxy-sdk",
-    version="1.1.1",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
         "psycopg2-binary",
